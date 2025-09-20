6082bccbb07681802e1be32426743326fe37e5e8
lutfiwijhaya <lutfiwijhaya@gmail.com>
2025-09-21 01:50:18 +0700

    21-09-2025

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
+
 date_default_timezone_set('Asia/Jakarta');
 
 $config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'].

d61b714e8748aaae88cbb1e41892d7c67fb7f889
lutfiwijhaya <lutfiwijhaya@gmail.com>
2025-09-21 02:43:16 +0700

    21-09-2025 01

--- application/config/config.php
+++ application/config/config.php
@@ -27,9 +27,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 date_default_timezone_set('Asia/Jakarta');
 
-$config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'].
-                        str_replace(basename($_SERVER['SCRIPT_NAME']),"",
-                        $_SERVER['SCRIPT_NAME']);
+$base  = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
+$base .= "://".$_SERVER['HTTP_HOST'];
+$base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$base = str_replace("index.php/","",$base);
+
+$config['base_url'] = rtrim($base, '/').'/';
+
 
 /*
 |--------------------------------------------------------------------------
